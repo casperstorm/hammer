a14f68dce3c41136fb85a74d2f082593a83f49ee
Casper Rogild Storm <2248455+casperstorm@users.noreply.github.com>
2020-12-08 14:58:08 +0100

    Update Hammer.lua

--- Hammer.lua
+++ Hammer.lua
@@ -1,24 +1,23 @@
-function hammer()
-  local repairAllCost, canRepair = GetRepairAllCost()
-  local money = GetMoney()
+function Hammer()
+    local repairAllCost, canRepair = GetRepairAllCost()
+    local money = GetMoney()
 
-  if (canRepair == true) then
-    if (repairAllCost <= money) then
-      RepairAllItems()
-      DEFAULT_CHAT_FRAME:AddMessage("Equipment was repaired for " .. GetCoinText(repairAllCost,", "));
-    else
-      DEFAULT_CHAT_FRAME:AddMessage("Insufficient funds to repair");
+    if (canRepair == true) then
+        if (repairAllCost <= money) then
+            RepairAllItems()
+            DEFAULT_CHAT_FRAME:AddMessage(
+                "Equipment was repaired for " ..
+                    GetCoinText(repairAllCost, ", "));
+        else
+            DEFAULT_CHAT_FRAME:AddMessage("Insufficient funds to repair");
+        end
     end
-  end
 end
 
-function event(self, event)
-  if event == "MERCHANT_SHOW" and CanMerchantRepair() then
-      Hammer()
-  end
+function OnEvent(self, event)
+    if event == "MERCHANT_SHOW" and CanMerchantRepair() then Hammer() end
 end
-
 
 local f = CreateFrame("FRAME")
 f:RegisterEvent("MERCHANT_SHOW")
-f:SetScript("OnEvent", event)
+f:SetScript("OnEvent", OnEvent)
