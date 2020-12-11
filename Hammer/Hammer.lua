e791bd7d3f7e229bae429a3d06bf08d992d8f759
Casper Rogild Storm <2248455+casperstorm@users.noreply.github.com>
2020-12-11 13:42:05 +0100

    chore: cleaned code

--- Hammer/Hammer.lua
+++ Hammer/Hammer.lua
@@ -6,7 +6,8 @@ local function Hammer()
         if repairAllCost <= money then
             RepairAllItems()
             DEFAULT_CHAT_FRAME:AddMessage(
-                "Equipment was repaired for " .. FormatMoney(repairAllCost))
+                "Equipment was repaired for " ..
+                    GetCoinTextureString(repairAllCost))
         else
             DEFAULT_CHAT_FRAME:AddMessage("Insufficient funds to repair")
         end
@@ -15,17 +16,6 @@ end
 
 local function OnEvent(self, event)
     if event == "MERCHANT_SHOW" and CanMerchantRepair() then Hammer() end
-end
-
-local function FormatMoney(amount)
-    local gold = floor(amount / 100 / 100)
-    local silver = floor((amount / 100) % 100)
-    local copper = amount % 100
-    local text = ""
-    if gold > 0 then text = "|cffffd700" .. gold .. "g|r" end
-    if silver > 0 then text = text .. "|cffc7c7cf" .. silver .. "s|r" end
-    if copper > 0 then text = text .. "|cffeda55f" .. copper .. "c|r" end
-    return text ~= "" and text or "none"
 end
 
 local f = CreateFrame("FRAME")
